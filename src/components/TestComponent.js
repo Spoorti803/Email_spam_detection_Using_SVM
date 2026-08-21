1e8d21073abae119f5725557c23bed2e3a68d25c
Spoorti803 <166362650+Spoorti803@users.noreply.github.com>
2026-08-21 11:38:53 +0530

    Delete src/components/TestComponent.js

--- src/components/TestComponent.js
+++
@@ -1,8 +0,0 @@
-// // src/components/TestComponent.js
-// import React from 'react';
-
-// const TestComponent = () => {
-//     return <div>This is a test component. Everything is working fine!</div>;
-// };
-
-// export default TestComponent;
